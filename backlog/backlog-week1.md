bd394c7e02a432914f0292f12fa760019accac3d
Jaswanth Bommidi <jassu.bommidi@gmail.com>
2021-04-15 07:02:00 +0530

    Add Backlog and Unit Tests

---
+++ backlog/backlog-week1.md
@@ -0,0 +1,6 @@
+| Task | Who  |  Status  | Expected Date  | 
+|---|---|---|---|
+| Complete evaluator routine  | Jaswanth  | Completed  |  April 9 |  
+| Init scripts, DB & logger initialisation  | Suraj  |  Completed |  April 6 |  
+|  Routine for registering user in DB | Akhil  | Completed  | April 7  | 
+|  Routine for retrieving a user in DB | Keshav  | Completed  | April 8  | 
---
+++ tests/main.py
@@ -0,0 +1,29 @@
+from core.pundit.pundit import Pundit
+
+tester = Pundit()
+
+class User:
+    self.id = 1111
+    self.name = "lalla"
+
+class testPundit:
+    def __init__(tester, user):
+        self.tester = tester
+        self.user = user
+        self.testuser = User()
+
+        self.tester.register(user=user)
+        tester.register(user=user)
+        self.tester.getName(id_=testuser.id)
+        self.tester.evaluate(user=testuser, link="https://bashupload.com/aFiur/submission.zip", ticket="001_aeivub_aqeorequb")
+
+        
+
+testuser = User()
+testPundit(tester, testuser)
+
+
+
+
+
+
